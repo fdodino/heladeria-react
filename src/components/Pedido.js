5ba7d6453265c426b780d8155d540e124774a026
fdodino <fdodino@10pines.com>
2019-07-17 11:52:06 -0300

    WIP: state class

--- src/components/Pedido.js
+++ src/components/Pedido.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React from 'react'
+import {orden} from '../domain/orden'
 
 export function Gusto(props) {
     const color = Math.floor(Math.random() * 3)
@@ -11,7 +12,7 @@ export function Gusto(props) {
 
 export function Pedido(props) {
     return (
-        <div class="fila">
+        <div className="fila">
             <div className="columnaPedido">
                 <div className="pino">{props.pedido.pino}</div>
                 <div className="gustos">{props.pedido.gustos.map(gusto => <Gusto gusto={gusto}/>)}</div>
@@ -23,17 +24,85 @@ export function Pedido(props) {
     )
 }
 
-export function TablaPedido(props) {
-    return (
-        <div className="main">
-            <h1>Pinohelados</h1>
-            <div className="contenedor">
-                <div className="encabezado">
-                    <div className="columnaPedido">Pedido</div>
-                    <div className="columnaPago">¿Pagó?</div>
+export class TablaPedido extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            orden,
+        }
+        this.agregarPedido.bind(this.agregarPedido)
+    }
+
+    agregarPedido({ pino, gusto1, gusto2 }) {
+        const orden = {
+            ...this.state.orden
+        }
+        orden.agregarPedido(pino, [gusto1, gusto2])
+        this.setState({
+            orden,
+        })
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <h1>Pinohelados</h1>
+                <div className="contenedor">
+                    <div className="encabezado">
+                        <div className="columnaPedido">Pedido</div>
+                        <div className="columnaPago">¿Pagó?</div>
+                    </div>
+                    {this.props.orden.pedidos.map(pedido => <Pedido pedido={pedido} key={pedido.pino}/>)}
                 </div>
-                {props.orden.pedidos.map(pedido => <Pedido pedido={pedido} key={pedido.pino}/>)}
+                <div className="filtro">
+                    <NuevoPedido onNewPedido={this.agregarPedido}/>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+}
+
+class NuevoPedido extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            pino: "",
+            gusto1: "Chocolate",
+            gusto2: "",
+        }
+    }
+
+    cambiarPino(pino) {
+        this.setState({
+            ...this.state,
+            pino,
+        })
+    }
+
+    cambiarGusto1(gusto1) {
+        this.setState({
+            ...this.state,
+            gusto1: gusto1,
+        })
+    }
+
+    cambiarGusto2(gusto2) {
+        this.setState({
+            ...this.state,
+            gusto2: gusto2,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" placeholder="pino" onChange={(e) => this.cambiarPino(e.target.value)} value={this.state.pino}/>
+                <input type="text" placeholder="gusto1" onChange={(e) => this.cambiarGusto1(e.target.value)} value={this.state.gusto1} />
+                <input type="text" placeholder="gusto2" onChange={(e) => this.cambiarGusto2(e.target.value)} value={this.state.gusto2} />
+                <button onClick={() => this.props.onNewPedido(this.state)}>Pedir!</button>
+            </div>
+        )
+    }
 }
